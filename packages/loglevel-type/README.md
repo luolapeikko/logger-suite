dd1bf0a9ef1d9348e8bb5b7ab3b55554fb2d7309
Marko Harjula <marko.harjula@gmail.com>
2026-07-25 17:18:36 +0300

    updates

---
+++ packages/loglevel-type/README.md
@@ -0,0 +1,7 @@
+# @luolapeikko/loglevel-type
+
+## Overview
+
+Generic log level type definition for TypeScript.
+
+See more in [documentation](https://luolapeikko.github.io/logger-suite/)
---
+++ packages/loglevel-type/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * LogLevelType is a type definition for log levels used in logging libraries.
+ * It defines the possible log levels that can be used to categorize log messages.
+ * @example
+ * import type { LogLevelType } from '@luolapeikko/loglevel-type';
+ * const level: LogLevelType = 'info';
+ * @since v0.0.1
+ * @see [LogLevelType](https://luolapeikko.github.io/logger-suite/types/_luolapeikko_loglevel-type.LogLevelType.html)
+ */
+export type LogLevelType = 'none' | 'trace' | 'debug' | 'info' | 'warn' | 'error';
+
