9d31f36f88e9831193da66e1f50dda9777b3e9a9
Marko Harjula <marko.harjula@gmail.com>
2026-07-25 15:00:16 +0300

    initial

---
+++ packages/logger-type/index.d.ts
@@ -0,0 +1,18 @@
+/**
+ * [ILoggerLike](https://luolapeikko.github.io/logger-suite/interfaces/ILoggerLike.html) is a common logger interface which should work with console, winston, and log4js.
+ * @example
+ * import {type ILoggerLike} from '@luolapeikko/logger-like';
+ * function demo(logger: ILoggerLike) {
+ *	 logger.info('hello');
+ * }
+ * demo(console);
+ * @since v0.0.1
+ * @see [ILoggerLike](https://luolapeikko.github.io/logger-suite/interfaces/ILoggerLike.html)
+ */
+export interface ILoggerLike {
+	trace?(message: any, ...args: any[]): void;
+	debug(message: any, ...args: any[]): void;
+	info(message: any, ...args: any[]): void;
+	warn(message: any, ...args: any[]): void;
+	error(message: any, ...args: any[]): void;
+}
