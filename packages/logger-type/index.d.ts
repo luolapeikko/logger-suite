d754ffb5775fc2057d0e5bf7f5bcdad9c0518644
Marko Harjula <marko.harjula@gmail.com>
2026-07-27 09:27:20 +0300

    updates to LoggerPlugin api

--- packages/logger-type/index.d.ts
+++ packages/logger-type/index.d.ts
@@ -28,9 +28,9 @@ export type LogArgs = [message: any, ...args: any[]];
 export type LogMethod = 'debug' | 'info' | 'warn' | 'error' | 'trace';
 /**
  * [LoggerPlugin](https://luolapeikko.github.io/logger-suite/interfaces/_luolapeikko_logger-type.LoggerPlugin.html) is a plugin interface for loggers that allows you to modify log messages before they are logged.
- * @since v0.0.3
+ * @since v0.0.6
  * @see [LoggerPlugin](https://luolapeikko.github.io/logger-suite/interfaces/_luolapeikko_logger-type.LoggerPlugin.html)
  */
 export interface LoggerPlugin {
-	handle(level: LogMethod, ...args: LogArgs): LogArgs;
+	handle(level: LogMethod, ...args: LogArgs): LogArgs | undefined;
 }
